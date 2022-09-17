b1a919d137c6da78dfe229882ad676e1fb7ea9f3
romanvergunov <romanvergunov11@gmail.com>
2022-09-18 01:07:51 +0600

    add animated greensock and preloader in website

--- src/js/components/_animation.js
+++ src/js/components/_animation.js
@@ -3,7 +3,7 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const t1 = gsap.timeline();
 t1.fromTo('.site-hero__container', { x: -30 }, { x: 0 });
-t1.fromTo('.site-about', { x: 30 }, { x: 0 });
+t1.fromTo('.site-about__container', { x: 30 }, { x: 0 });
 t1.fromTo('.site-projects__list', { x: -30 }, { x: 0 });
 t1.fromTo('.contacts-left', { x: -30 }, { x: 0 });
 t1.fromTo('.contacts-right', { x: 30 }, { x: 0 });
